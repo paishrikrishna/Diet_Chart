193ee2405ca557fe703ec67b43ef1f6a9b431dbc
paishrikrishna <43004882+paishrikrishna@users.noreply.github.com>
2020-10-26 18:50:01 +0530

    Update models.py

--- diet_chart/homepage/models.py
+++ diet_chart/homepage/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class meal_distribution(models.Model):
-	User_no = models.TextField(default='1')
+	User_no = models.TextField(default='n/a')
+	Date = models.TextField(default='N/A')
 	Meal_names = models.TextField(default='N/A')
 	Meal_times = models.TextField(default='N/A')
 	Meal_calories = models.TextField(default='N/A')
